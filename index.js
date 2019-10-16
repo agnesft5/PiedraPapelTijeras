2af580972a3877e16bb08fc32d9c9e16912eeea6
Agnes Font <agnesft5@gmail.com>
2019-10-16 20:41:21 +0200

    Añadido botón return

--- index.js
+++ index.js
@@ -1,38 +1,48 @@
 function letsPlay(string1,string2){
     if (string1 === string2){
         return("Ha habido empate!")
-    }else if(string1 === "papel" && string2 === "piedra"){
+    }else if(string1 === "Papel" && string2 === "Piedra"){
         return("El ordenador gana");
-    }else if(string1 === "piedra" && string2 === "tijeras"){
+    }else if(string1 === "Piedra" && string2 === "Tijeras"){
         return("El ordenador gana");
-    }else if(string1 ==="tijeras" && string2 ==="papel"){
+    }else if(string1 ==="Tijeras" && string2 ==="Papel"){
         return("El ordenador gana");
-    }else if(string1 === "piedra" && string2 === "papel"){
+    }else if(string1 === "Piedra" && string2 === "Papel"){
         return("Has ganado!");
-    }else if(string1 === "papel" && string2 === "tijeras"){
+    }else if(string1 === "Papel" && string2 === "Tijeras"){
         return("Has ganado!");
-    }else if(string1 ==="tijeras" && string2 ==="piedra"){
+    }else if(string1 ==="Tijeras" && string2 ==="Piedra"){
         return("Has ganado!");
     }else{
         return("Vuelve a jugar!")
     }
 }
 
-    let array = ["piedra","papel","tijeras"];
+    let array = ["Piedra","Papel","Tijeras"];
+
+
+function goBack (){
+    location.reload(true)
+}
+
 
 
 //on vull que vagi el resultat
 let outputContainerPC = document.querySelector(".result__textpc")
 let outputContainer = document.querySelector(".result__text");
+let returnButton = document.querySelector(".result__button");
 
 //botó pedra
 let piedraButton = document.querySelectorAll(".piedra__button")[0];
 piedraButton.addEventListener("click",()=>{
     let index = Math.round(Math.random()*(array.length-1));
     let string1 = array[index];
-    let playingPiedra = letsPlay(string1,"piedra")
+    let playingPiedra = letsPlay(string1,"Piedra")
     outputContainerPC.innerHTML = `El ordenador ha sacado: ${string1}`
     outputContainer.innerHTML =`${playingPiedra}`
+    returnButton.className = ["btn btn-secondary m-3 result__button"];
+    returnButton.addEventListener("click",()=>{goBack()
+    });
 });
 
 //botó paper
@@ -40,9 +50,12 @@ let papelButton = document.querySelectorAll(".papel__button")[0];
 papelButton.addEventListener("click",()=>{
     let index = Math.round(Math.random()*(array.length-1));
     let string1 = array[index];
-    let playingPapel = letsPlay(string1,"papel")
+    let playingPapel = letsPlay(string1,"Papel")
     outputContainerPC.innerHTML = `El ordenador ha sacado: ${string1}`
     outputContainer.innerHTML = `${playingPapel}`
+    returnButton.className = ["btn btn-secondary m-3 result__button"];
+    returnButton.addEventListener("click",()=>{goBack()
+    });
 });
 
 //botó tisores
@@ -50,7 +63,10 @@ let tijerasButton = document.querySelectorAll(".tijeras__button")[0];
 tijerasButton.addEventListener("click",()=>{
     let index = Math.round(Math.random()*(array.length-1));
     let string1 = array[index];
-    let playingTijeras= letsPlay(string1,"tijeras")
+    let playingTijeras= letsPlay(string1,"Tijeras")
     outputContainerPC.innerHTML = `El ordenador ha sacado: ${string1}`
     outputContainer.innerHTML = `${playingTijeras}`
+    returnButton.className = ["btn btn-secondary m-3 result__button"];
+    returnButton.addEventListener("click",()=>{goBack()
+    });
 });
